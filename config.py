b9d4a637031f6e889859643219552fba7a175c01
Thanh Trung <nguyenthanhtrung220779@gmail.com>
2026-07-15 19:22:32 +0700

    Update: Cap nhat danh sach hoc sinh

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+problem_folder="problems"
+submission_folder="submissions"
+time_limit=2
+# Cấu hình thêm cho V1.1 C++
+build_folder = "build"  # Nơi chứa các file .exe sau khi biên dịch
+# --- TÍNH NĂNG MỚI V1.2: QUẢN LÝ NGƯỜI DÙNG ---
+USERS = {
+    "teacher1": {"password": "123", "role": "admin", "name": "Thầy Trung"},
+    "hocsinhA": {"password": "abc", "role": "student", "name": "Nguyễn Hồng Huy"},
+    "hocsinhB": {"password": "xyz", "role": "student", "name": "Trần Thị B"}
+}
+# --- TÍNH NĂNG MỚI V1.3: LƯU LỊCH SỬ CHẤM ---
+history_file = "history.csv"
